fabdc56caeba79e325d215fe738f6328e45b04eb
KillMoon <dande235@gmail.com>
2021-02-19 01:26:47 +0700

    add practice 2

--- lab1/main.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
-import numpy as np
-
-if __name__ == '__main__':
-    data = pd.read_csv('BankChurners.csv', delimiter=',')
-    print(data.head())
